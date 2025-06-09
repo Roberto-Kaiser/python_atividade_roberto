abf943034b0128e223fdfc7b9e62e497c59136f3
Roberto-Kaiser <159036437+Roberto-Kaiser@users.noreply.github.com>
2025-06-09 07:38:13 -0300

    Create meusdados.py

---
+++ meusdados.py
@@ -0,0 +1,8 @@
+senha = input("Digite sua senha:")
+if senha == "Robertao2008#":
+    print("Seu dados:\nCpf: 111.111.111-11\nEmail: robisvaldo@gmail.com\nGênero: Masculino\nTelefone: (41)95565-1788")
+else:
+    print("Digite a sua senha corretamente para acessar seus dados!")
+    senha = input("Digite sua senha:")
+    if senha == "Robertao2008#":
+        print("Seu dados:\nCpf: 111.111.111-11\nEmail: robisvaldo@gmail.com\nGênero: Masculino\nTelefone: (41)95565-1788")
